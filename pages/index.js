182fe7f274abab4cd45273882db63dd581741b0a
Fabricio <rocha.academico@live.com>
2026-02-27 01:16:38 -0300

    Incluso versiculo 16

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 function Home(){
-    return <h2>Isaías 49:15 - Ainda que uma mãe viesse a se esquecer de um filho que ainda mama, eu todavia não me esquecerei de ti, diz o Senhor.</h2>}
+    return <h2>Isaías 49 - 15 - Ainda que uma mãe viesse a se esquecer de um filho que ainda mama, eu todavia não me esquecerei de ti, diz o Senhor.
+                           16 - Eis que, eu te tenho gravado nas palmas de minhas mãos; teus muros estão continuamente diante de mim.</h2>
+}
 
 export default Home;
