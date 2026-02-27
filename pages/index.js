50906b9e965e868d229f35829fb9f61c83086c4b
Fabricio <rocha.academico@live.com>
2026-02-27 00:22:49 -0300

    Completado o versiculo (corrigido mensagem).

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 function Home(){
-    return <h2>Isaías 49:15 - Ainda que uma mãe viesse a se esquecer de um filho que ainda mama, eu todavia não me esquecerei de ti</h2>
-}
+    return <h2>Isaías 49:15 - Ainda que uma mãe viesse a se esquecer de um filho que ainda mama, eu todavia não me esquecerei de ti, diz o Senhor.</h2>}
 
 export default Home;
